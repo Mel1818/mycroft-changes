2bbc9ebb133965b87a70bfeedafd83054a9e782e
melissabell <bellm1us@gmail.com>
2019-09-15 14:27:37 -0400

    decouple backend

--- mycroft/backend/Configuration.py
+++ mycroft/backend/Configuration.py
@@ -5,17 +5,20 @@ from mycroft.backend.utils import load_commented_json
 USER_CONFIG = join(expanduser('~'), 'mycroft-changes/mycroft/configuration/mycroft.conf')
 
 
-def load_local(self):
+def load_local():
 
     if exists(USER_CONFIG) and isfile(USER_CONFIG):
         try:
+            result = {}
             config = load_commented_json(USER_CONFIG)
             for key in config:
-                self.__setitem__(key, config[key])
+                result[key] =config[key]
 
             LOG.debug("Configuration {} loaded".format(USER_CONFIG))
         except Exception as e:
             LOG.error("Error loading configuration '{}'".format(USER_CONFIG))
             LOG.error(repr(e))
     else:
+
         LOG.debug("Configuration '{}' not defined, skipping".format(USER_CONFIG))
+    return result
