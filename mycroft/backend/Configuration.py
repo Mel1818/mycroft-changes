94334d65df02a9e0ddb1b8705d86a97045c6d100
melissabell <bellm1us@gmail.com>
2019-09-18 08:52:41 -0400

    merge

--- mycroft/backend/Configuration.py
+++ mycroft/backend/Configuration.py
@@ -10,7 +10,7 @@ USER_CONFIG = join(expanduser('~'), '.mycroft/mycroft.conf')
 
 class LocalConfig:
     def __init__(self):
-        super(LocalConfig, self).__init__(None)
+        super(LocalConfig, self).__init__()
         mycroft.configuration.Configuration.load_config_stack([{}], True)
         local = mycroft.configuration.LocalConf(USER_CONFIG)
         config = mycroft.configuration.Configuration.load_config_stack([local], True)
